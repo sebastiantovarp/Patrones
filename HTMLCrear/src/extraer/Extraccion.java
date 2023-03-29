5cd20276a2bf31c20544bbfd36f2c1d3b3b4799d
alejo28042001 <diegobermu1003@gmail.com>
2023-03-28 21:03:54 -0500

    Se crea la funcionalidad de extraer

---
+++ HTMLCrear/src/extraer/Extraccion.java
@@ -0,0 +1,8 @@
+package extraer;
+
+public class Extraccion {
+    public String extraer(String texto,int indexInicial,int indexFinal)
+    {
+        return texto.substring(indexInicial,indexFinal);
+    }
+}
